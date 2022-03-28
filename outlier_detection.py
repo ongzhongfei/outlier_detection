2ad95891a9dea9bbc873b9f9d146deba53c6bb35
Ong Zhong Fei <zhongfei@bnm.gov.my>
2022-03-28 18:58:10 +0800

    updated outlier

--- outlier_detection.py
+++ outlier_detection.py
@@ -7,6 +7,8 @@ from pandas.io.json import json_normalize
 import statistics
 from scipy import stats
 import plotly.express as px
+import plotly.graph_objects as go
+
 from sklearn.cluster import DBSCAN
 from sklearn.ensemble import IsolationForest
 
@@ -107,7 +109,7 @@ def loaddata3():
     dataset = pd.DataFrame(list(zip(xdate, df['gdp_growth'], x, x2, x3, x4, x5, x6, x7, x8, x9, x10,
                                 x11, x12, x13, x14, x15, x16, x17, x18, x19, x20,
                                 x21, x22, x23, x24, x25, x26, x27, x28)), 
-                        columns = ['date', 'gdp_growth', 'all_division', 'mining', 'electricity', 'manufacturing',
+                        columns = ['date', 'gdp_growth', 'IPI All', 'mining', 'electricity', 'manufacturing',
                                     'exp_ind_tot', 'exp_ind_ele_clu_tot', 'exp_ind_ele_clu_ele', 'exp_ind_ele_clu_ele_pro', 
                                     'exp_ind_ele_clu_mac', 'exp_ind_pri_clu_tot', 'exp_ind_pri_clu_che', 'exp_ind_pri_clu_pet', 
                                     'exp_ind_pri_clu_tex','exp_ind_pri_clu_woo', 'exp_ind_pri_clu_rub', 'exp_ind_pri_clu_off',
@@ -116,8 +118,11 @@ def loaddata3():
                                     'dom_ind_csm_clu_tra', 'dom_ind_csm_clu_bev', 'dom_ind_csm_clu_tob', 'dom_ind_csm_clu_oth'])
     dataset['date'] = dataset['date'].sort_values(ascending=True).values
     dataset.drop(dataset.tail(1).index,inplace=True)
+    qs = dataset['date'].str.replace(r'(Q\d) (\d+)', r'\2-\1')
+    dataset['datetime'] = pd.PeriodIndex(qs, freq='Q').to_timestamp()
     return dataset
 dataset = loaddata3()
+
 
 if st.checkbox('Show raw data'):
     st.subheader('GDP and IPI data')
@@ -129,7 +134,7 @@ st.table(dataset.describe())
 with st.expander("See variables"):
      st.write("""
         - gdp_growth : Gross domestic product (GDP)
-        - all_division : All divisions
+        - all_division : IPI All
         - mining : Mining
         - electricity : Electricity
         - manufacturing : Manufacturing
@@ -167,7 +172,7 @@ st.subheader('1. Traditional method - Measures of Variability')
 """
 option = st.selectbox(
      'Choose 1 variable.',
-     ('gdp_growth', 'all_division', 'mining', 'electricity', 'manufacturing',
+     ('gdp_growth', 'IPI All', 'mining', 'electricity', 'manufacturing',
         'exp_ind_tot', 'exp_ind_ele_clu_tot', 'exp_ind_ele_clu_ele', 'exp_ind_ele_clu_ele_pro', 
         'exp_ind_ele_clu_mac', 'exp_ind_pri_clu_tot', 'exp_ind_pri_clu_che', 'exp_ind_pri_clu_pet', 
         'exp_ind_pri_clu_tex','exp_ind_pri_clu_woo', 'exp_ind_pri_clu_rub', 'exp_ind_pri_clu_off',
@@ -201,7 +206,7 @@ Scatter plots and box plots are the most preferred visualization tools to detect
 """
 option2 = st.selectbox(
      'Choose 1 variable:',
-     ('gdp_growth', 'all_division', 'mining', 'electricity', 'manufacturing',
+     ('gdp_growth', 'IPI All', 'mining', 'electricity', 'manufacturing',
         'exp_ind_tot', 'exp_ind_ele_clu_tot', 'exp_ind_ele_clu_ele', 'exp_ind_ele_clu_ele_pro', 
         'exp_ind_ele_clu_mac', 'exp_ind_pri_clu_tot', 'exp_ind_pri_clu_che', 'exp_ind_pri_clu_pet', 
         'exp_ind_pri_clu_tex','exp_ind_pri_clu_woo', 'exp_ind_pri_clu_rub', 'exp_ind_pri_clu_off',
@@ -209,12 +214,50 @@ option2 = st.selectbox(
         'dom_ind_con_clu_iro', 'dom_ind_con_clu_fab','dom_ind_csm_clu_tot', 'dom_ind_csm_clu_foo', 
         'dom_ind_csm_clu_tra', 'dom_ind_csm_clu_bev', 'dom_ind_csm_clu_tob', 'dom_ind_csm_clu_oth'))
 
-graph1 = px.scatter(x=dataset['date'] , y=dataset[option2])
-graph1.update_layout(
+graph1 = px.box(dataset, x=option2)
+
+
+iqr = stats.iqr(dataset[option2], rng=(25, 75))
+min_fence = np.percentile(dataset[option2], 25) - (1.5*iqr)
+max_fence = np.percentile(dataset[option2], 75) + (1.5*iqr)
+
+# graph1 = px.scatter(x=dataset['date'] , y=dataset[option2])
+graph2 = go.Figure(data=go.Scatter(x=dataset['datetime'], y=dataset[option2], mode='lines+markers'))
+graph2.add_shape(
+        type='line',
+        x0=dataset['datetime'].min(),
+        y0=min_fence,
+        x1=dataset['datetime'].max(),
+        y1=min_fence,
+        line=dict(
+            color='Red',
+        ),
+        opacity =0.3
+)
+graph2.add_shape(
+        type='line',
+        x0=dataset['datetime'].min(),
+        y0=max_fence,
+        x1=dataset['datetime'].max(),
+        y1=max_fence,
+        line=dict(
+            color='Red',
+        ),
+        opacity =0.3
+)
+graph2.add_annotation(x=dataset['datetime'].max(), y=max_fence,
+            text=str(round(max_fence,1)),
+            font = {'color':'red'},
+            showarrow=False,yshift=10)
+graph2.add_annotation(x=dataset['datetime'].max(), y=min_fence,
+            text=str(round(min_fence,1)),
+            font = {'color':'red'},
+            showarrow=False,yshift=10)
+graph2.update_layout(
         xaxis_title= "Date",
+        yaxis_title= option2,
     ) 
 
-graph2 = px.box(x=dataset[option2])
 
 col1, col2 = st.columns(2)
 
@@ -233,11 +276,21 @@ Machine learning for anomaly detection includes techniques that provide a promis
 and classification of anomalies based on an initially large set of features. Using the GDP data we will 
 focus on using 2 machine learning techniques.
 """)
+# Separate the df from variable (datatype date format)
 
-df_dbscan = dataset.copy()
-df_if = dataset.copy()
-
-st.subheader('- Density-based spatial clustering of applications with noise (DBSCAN)​')
+with st.form("Outlier variables selection: "):
+    option3 = st.multiselect(
+        'Choose variables detect outlier: (Please select at least 2 variables)',
+        ['gdp_growth', 'IPI All', 'mining', 'electricity', 'manufacturing',
+            'exp_ind_tot', 'exp_ind_ele_clu_tot', 'exp_ind_ele_clu_ele', 'exp_ind_ele_clu_ele_pro', 
+            'exp_ind_ele_clu_mac', 'exp_ind_pri_clu_tot', 'exp_ind_pri_clu_che', 'exp_ind_pri_clu_pet', 
+            'exp_ind_pri_clu_tex','exp_ind_pri_clu_woo', 'exp_ind_pri_clu_rub', 'exp_ind_pri_clu_off',
+            'exp_ind_pri_clu_pap', 'dom_ind_tot', 'dom_ind_con_clu_tot', 'dom_ind_con_clu_non', 
+            'dom_ind_con_clu_iro', 'dom_ind_con_clu_fab','dom_ind_csm_clu_tot', 'dom_ind_csm_clu_foo', 
+            'dom_ind_csm_clu_tra', 'dom_ind_csm_clu_bev', 'dom_ind_csm_clu_tob', 'dom_ind_csm_clu_oth'],
+        default = ['gdp_growth' , 'IPI All'])
+    submitted = st.form_submit_button("Submit")
+st.subheader('3.1 Density-based spatial clustering of applications with noise (DBSCAN)​')
 st.info("""
 DBSCAN is able to find arbitrary shaped clusters and clusters with noise (i.e. outliers).
 The main idea behind DBSCAN is that a point belongs to a cluster if it is close to many points from that cluster.
@@ -262,26 +315,38 @@ Cons:
 col4, col5= st.columns(2)
 
 with col4:
-    eps1 = st.slider('Choose the eps value:', min_value = 5, max_value= 50, value= 5, step=1 )
+    eps1 = st.slider('Choose the eps value:', min_value = 1, max_value= 30, value= 3, step=1 )
 with col5:
     minsample1 = st.slider('Choose the min sample:', min_value = 3, max_value= 10, value= 3 )
 
 
-# Separate the df from variable (datatype date format)
-df_model = dataset.drop(['date'], axis=1)
 
-model = DBSCAN(eps = eps1, min_samples =minsample1).fit(df_model)
+
+#df_model = dataset.drop(['date'], axis=1)
+df_dbscan = dataset.copy()
+df_if = dataset.copy()
+
+model = DBSCAN(eps = eps1, min_samples =minsample1).fit(dataset[option3])
 colors = model.labels_
-color_discrete_map = {'rgb(255,0,0)', 'rgb(0,255,0)', 'rgb(0,0,255)'}
-graph4 = px.scatter(dataset['gdp_growth'], dataset['all_division'], color =colors, color_continuous_scale=["red", "green", "blue"])
+df_dbscan["dbscan_outliers"] = model.fit_predict(dataset[option3])
+df_dbscan['anomaly']= df_dbscan['dbscan_outliers'].apply(lambda x: 'outlier' if x==-1  else 'normal')
+# graph4 = px.scatter(dataset['gdp_growth'], dataset['all_division'], color =df_dbscan['anomaly'])
+if len(option3) == 2:
+    graph4 = px.scatter(dataset, x=option3[0], y = option3[1], color =df_dbscan['anomaly'])
+    graph4.update_traces(marker={'size': 15})
+else:
+    graph4 = px.scatter_3d(dataset,x=option3[0],
+                        y=option3[1],
+                        z=option3[2],
+                        color=df_dbscan['anomaly'])
+
 
 st.plotly_chart(graph4 , use_container_width=True)
 
-df_dbscan["dbscan_outliers"] = model.fit_predict(df_model)
-
+st.write('Below are the identified outliers:')
 st.write(df_dbscan[df_dbscan["dbscan_outliers"]==-1])
 
-st.subheader('- Isolation forest')
+st.subheader('3.2 Isolation forest')
 st.info("""
 Isolation forests are built using decision trees. 
 
@@ -296,16 +361,21 @@ Note: scikit-learn’s isolation forest introduced a modification of the anomaly
 The outliers are indicated by negative scores, while the positive score implies that we have normal observation.
 """)
 
-model2=IsolationForest(n_estimators=100,max_samples='auto',contamination=float(0.2)).fit(df_model)
-df_if['scores'] = model2.decision_function(df_model)
-df_if['if_outliers'] = model2.predict(df_model)
+model2=IsolationForest(n_estimators=100,max_samples='auto',contamination=float(0.2)).fit(dataset[option3])
+df_if['scores'] = model2.decision_function(dataset[option3])
+df_if['if_outliers'] = model2.predict(dataset[option3])
 df_if['anomaly']=df_if['if_outliers'].apply(lambda x: 'outlier' if x==-1  else 'normal')
 
-graph5 = px.scatter_3d(df_if,x='all_division',
-                       y='mining',
-                       z='electricity',
-                       color='anomaly')
+if len(option3) == 2:
+    graph5 = px.scatter(df_if, x=option3[0], y = option3[1], color =df_dbscan['anomaly'])
+    graph5.update_traces(marker={'size': 15})
+
+else:
+    graph5 = px.scatter_3d(df_if,x=option3[0],
+                        y=option3[1],
+                        z=option3[2],
+                        color='anomaly')
 
 st.plotly_chart(graph5 , use_container_width=True)
-
+st.write('Below are the identified outliers:')
 st.write(df_if[df_if['if_outliers']==-1])
